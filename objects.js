4cf980c2f70da4e27eaf1d26fe8e678e4eb03925
Kailee Gray <kaileeagray@gmail.com>
2019-05-02 15:42:17 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,9 @@
 var playlist = {
+<<<<<<< HEAD
   "Pink Floyd": "Money"
+=======
+  "Pink Floyd": "Money",
+>>>>>>> 56d218be571907982a331a97ef27b2c2ac550118
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
@@ -8,6 +12,10 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
+<<<<<<< HEAD
   delete playlist[artistName];
   return playlist;
+=======
+
+>>>>>>> 56d218be571907982a331a97ef27b2c2ac550118
 }
